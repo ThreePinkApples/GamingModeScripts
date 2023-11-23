4695e6e9fab5e6e0696f3769ad99caadf641aa67
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2023-11-23 20:36:06 +0100

    Add set_pc_primary and set_tv_primary scripts

---
+++ set_pc_primary.ps1
@@ -0,0 +1,2 @@
+& $PSScriptRoot/toggle_main_display.ps1 -MonitorId ACR0490
+& $PSScriptRoot/toggle_audio.ps1
---
+++ set_tv_primary.ps1
@@ -0,0 +1,2 @@
+& $PSScriptRoot/toggle_main_display.ps1 -MonitorId GSM81CD
+& $PSScriptRoot/toggle_audio.ps1
