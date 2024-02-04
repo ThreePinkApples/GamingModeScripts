1ad7841e80c8fe5d5773e27df4ee9692bb0d8aa3
Øyvind Heddeland Instefjord <instefjord@outlook.com>
2024-02-04 11:24:35 +0100

    toggle_main_display: Handle 125% scaling on TV

--- toggle_main_display.ps1
+++ toggle_main_display.ps1
@@ -10,17 +10,16 @@ if ($MonitorId -ne "") {
 Add-Type -AssemblyName System.Windows.Forms
 $screens = [System.Windows.Forms.Screen]::AllScreens
 $primary = $screens | Where-Object {$_.Primary -eq 'True'}
-$tv = $screens | Where-Object {$_.Bounds.Width -eq 3840 -And $_.Bounds.Height -eq 2160}
-# This is the "Short Monitor ID" found by running MultiMonitorTool.exe
-$primaryGamingMonitorId = "ACR0490"
+# The Bounds.Width/.Height are not as simple as the resolution, but the scaled values after using scaling. So on a 4K screen with 125% scaling Width is 3072, not 3840.
+# If you use even higher scaling, such as 150%, this check will not work and you should hardcode the values
+$tv = $screens | Where-Object {$_.Bounds.Width -gt 2560 -And $_.Bounds.Height -gt 1440}
 if ($tv -eq $null) {
-    # My TV has a resolution of 3840x2160, but with UI scaling set to 200%, so that's what I'm checking for here.
-    # (My monitors are both 2560x1440)
     # You should change this value to match your TV and scaling combination. Run MultiMonitorTool, select your TV, and look for "Window Size" on the "Microsoft Text Input Application" row. That should be the Bounds.
     # If your TV and PC monitor has the same resolution you could try checking for refresh rate.
     $tv = $screens | Where-Object {$_.Bounds.Width -eq 1920 -And $_.Bounds.Height -eq 1080}
 }
-
+# This is the "Short Monitor ID" found by running MultiMonitorTool.exe
+$primaryGamingMonitorId = "ACR0490"
 if ($primary.DeviceName -eq $tv.DeviceName) {
     & $multiMonitor /SetPrimary $primaryGamingMonitorId
 }
